788dbbeec2b03945b3a4071caf7bae057a81d35f
Swifttech360 <71281505+Swifttech360@users.noreply.github.com>
2025-02-05 05:07:19 -0500

    Working Solution Found!
    Finally created a modular block of code that correctly counts the number of syllables in any given piece of text.
    
    The code works using the syllapy package. Essentially, it separates the text into individual words, then uses the  'syllapy._syllables' function for each word.  After a word is checked for it's number of syllables, it then initializes a variable to an integer which represents the number of syllables in that word.  This integer is then appended to a list of numbers, each of which representing a different word. finally, after all words have been checked and converted to list integers, the sum function is used to add all those integers and return the number of syllables in the text.

--- SyllableCounttImportTest.py
+++ SyllableCounttImportTest.py
@@ -1,18 +1,12 @@
 import syllapy
-
-sen1 = "The quick brown fox jumps over the lazy dog."
-print(syllapy._syllables(sen1))
-sen2 = "This sentence is an example of a simple and straightforward sentence"
-print(syllapy._syllables(sen2))
-sen3 = "It contains short words and has-a-clear-structure.-On-the other-hand, the intricacies of the\n"\
-        " English language can lead to more complex and challenging texts. "
-print(syllapy._syllables(sen3))
-sen4 = "Reading comprehension and text analysis are important skills for individuals of all ages and backgrounds."
-print(syllapy._syllables(sen4))
-
-print(f"\nSUM TOTAL:  "
-      f"{(syllapy._syllables(sen1) + syllapy._syllables(sen2) + syllapy._syllables(sen3) + syllapy._syllables(sen4))}")
-while True:
-    x = input('')
-    print(syllapy._syllables(x))
-
+textNumList = []
+words = 'The quick brown fox jumps over the lazy dog. This sentence is an example of a simple and straightforward ' \
+        'sentence. It contains short words and has a clear structure. On the other hand, the intricacies of the ' \
+        'English language can lead to more complex and challenging texts. Reading comprehension and text analysis ' \
+        'are important skills for individuals of all ages and backgrounds.'
+words = words.split()
+for word in words:
+    x = int(syllapy._syllables(word))
+    textNumList.append(x)
+syllables = sum(textNumList)
+print(syllables)
